27e5196969ba769f1efe618b9372cea9df7716f8
tarynblakemiller <yourfriendtaryn@gmail.com>
2021-04-27 14:14:37 -0400

    pushing project to repository

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+import "./index.css";
+import "./fonts/TESLA.ttf";
+
+ReactDOM.render(<App />, document.getElementById("root"));
